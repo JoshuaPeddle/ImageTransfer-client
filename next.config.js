5fdfbd5bdfc6146721b163e4e94f216293650b6a
Joshua Peddle <joshuapeddle128@gmail.com>
2023-05-12 21:57:24 -0230

    Use env for image urls

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [ '127.0.0.1', process.env.NEXT_PUBLIC_IMAGE_SERVER ],
+  },
 };
 module.exports = nextConfig;

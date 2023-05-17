f4051870121f11789b7e01419635d87329b909bf
Joshua Peddle <joshuapeddle128@gmail.com>
2023-05-17 00:47:52 -0230

    Serve a random image for the user

--- components/TFView.js
+++ components/TFView.js
@@ -40,12 +40,24 @@ export default function TFView() {
     if (!result) return;
     setImage(result);
   };
+  // THis function should send a GET request to the generator server to get a url for a random image
+  const fetchRandomImage = async () => {
+    const res = await fetch(API_URL+'random', {method: 'GET'});
+    const data = await res.json();
+    console.log(data.url);
+    setImage(data.url);
+  };
+  const loaderProp =({ src }) => {
+    return src;
+  };
   return (
     <div>
       <h1>TFView</h1>
       <LocalImageLoader setImage={setImage} />
-      {image ? <_Image src={image} width="256" height="256" alt="" /> : ''}
+      {image ? <_Image src={image} width="256" height="256" alt="" loader={loaderProp} unoptimized /> : ''}
       {result ? <_Image src={result} width="256" height="256" alt="" /> : ''}
+      <button onClick={fetchRandomImage} >Random</button>
+      <br/>
       <button onClick={() => predict('monet', image)} >Monet</button>
       <br/>
       <button onClick={() => predict('gogh', image)} >Gogh</button>
@@ -53,6 +65,7 @@ export default function TFView() {
       <button onClick={() => predict('upscale', image)} >Upscale</button>
       <br/>
       <button onClick={resultToImage} >Result to Image</button>
+
     </div>
   );
 }

5dfdac4a290f75a6125e5e3692607c98ca6f69ea
Joshua Peddle <joshuapeddle128@gmail.com>
2023-05-16 21:54:57 -0230

    Add Van Goh

--- components/TFView.js
+++ components/TFView.js
@@ -2,6 +2,7 @@
 import { useState } from 'react';
 import LocalImageLoader from './LocalImageLoader';
 import _Image from 'next/image';
+
 const API_URL = 'http://'+process.env.NEXT_PUBLIC_IMAGE_SERVER+':5000/';
 export default function TFView() {
   const [ image, setImage ] = useState(null);
@@ -35,17 +36,23 @@ export default function TFView() {
           .catch((err) => console.error(err));
       });
   };
+  const resultToImage = () => {
+    if (!result) return;
+    setImage(result);
+  };
   return (
     <div>
-
       <h1>TFView</h1>
-
       <LocalImageLoader setImage={setImage} />
       {image ? <_Image src={image} width="256" height="256" alt="" /> : ''}
       {result ? <_Image src={result} width="256" height="256" alt="" /> : ''}
       <button onClick={() => predict('monet', image)} >Monet</button>
-      <br></br>
-      <button onClick={() => predict('upscale', result)} >Upscale</button>
+      <br/>
+      <button onClick={() => predict('gogh', image)} >Gogh</button>
+      <br/>
+      <button onClick={() => predict('upscale', image)} >Upscale</button>
+      <br/>
+      <button onClick={resultToImage} >Result to Image</button>
     </div>
   );
 }

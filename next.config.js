b28130db5529f7ed6d9a4d31dd74dd6e72af73b7
Joshua Peddle <joshuapeddle128@gmail.com>
2023-05-25 13:33:54 -0230

    Remove unused domain

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: [ '127.0.0.1', process.env.NEXT_PUBLIC_IMAGE_SERVER, '*cyclegan.site', 'github.com' ],
+    domains: [ '127.0.0.1',  '*cyclegan.site', 'github.com' ],
   },
   output: 'standalone',
 };

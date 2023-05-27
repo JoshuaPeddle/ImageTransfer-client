559d8bc6e0e7aad92d0427c26f2deb45d33405bb
Joshua Peddle <joshuapeddle128@gmail.com>
2023-05-27 21:02:25 -0230

    Add experimental mp4 exporter

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,6 +10,7 @@ export default function Document() {
         <Main />
         <NextScript />
         <Script src="/js/gif.js"strategy='lazyOnload'  />
+        <Script src="//cdn.jsdelivr.net/npm/@ffmpeg/ffmpeg@0.11.6/dist/ffmpeg.min.js"strategy='lazyOnload'  />
       </body>
     </Html>
   );

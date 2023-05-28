8a13ba2c6d4668c1aa962d530c8f7c26e8201841
Joshua Peddle <joshuapeddle128@gmail.com>
2023-05-28 20:48:01 -0230

    Add outter div

--- components/imageView.js
+++ components/imageView.js
@@ -3,11 +3,13 @@ import ImagePlaceholder from './ImagePlaceholder';
 import styles from './imageView.module.css';
 export default function ImageView({image, result, loading}) {
   return (
-    <div className={styles.images}>
-      {image ? <Image priority={true}  id='src_img' src={image} width="384" height="256" quality={95} alt="" loader={({ src }) => {
-        return src; 
-      }} unoptimized /> : <ImagePlaceholder loading={true}/>}
-      {result ? <Image id='res_img' src={result} width="384" height="256" alt="" quality={95} /> : <ImagePlaceholder loading={loading}/>}
+    <div className={styles.imageContainer}>
+      <div className={styles.images}>
+        {image ? <Image priority={true}  id='src_img' src={image} width="384" height="256" quality={95} alt="" loader={({ src }) => {
+          return src; 
+        }} unoptimized /> : <ImagePlaceholder loading={true}/>}
+        {result ? <Image id='res_img' src={result} width="384" height="256" alt="" quality={95} /> : <ImagePlaceholder loading={loading}/>}
+      </div>
     </div>
   );
 }

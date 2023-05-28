0d4908173ec7c0dd62de59d9751bece93bd78605
Joshua Peddle <joshuapeddle128@gmail.com>
2023-05-28 14:05:47 -0230

    Isolate Imageview component

---
+++ components/imageView.js
@@ -0,0 +1,13 @@
+import Image from 'next/image';
+import ImagePlaceholder from './ImagePlaceholder';
+import styles from './imageView.module.css';
+export default function ImageView({image, result, loading}) {
+  return (
+    <div className={styles.images}>
+      {image ? <Image priority={true}  id='src_img' src={image} width="384" height="256" quality={95} alt="" loader={({ src }) => {
+        return src; 
+      }} unoptimized /> : <ImagePlaceholder loading={true}/>}
+      {result ? <Image id='res_img' src={result} width="384" height="256" alt="" quality={95} /> : <ImagePlaceholder loading={loading}/>}
+    </div>
+  );
+}

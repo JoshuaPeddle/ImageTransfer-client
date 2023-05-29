4273812b28600576e2b818610cd62206c1160732
Joshua Peddle <joshuapeddle128@gmail.com>
2023-05-28 21:52:07 -0230

    Add new placeholder images

--- components/ImagePlaceholder.js
+++ components/ImagePlaceholder.js
@@ -1,9 +1,13 @@
 import styles from './ImagePlaceholder.module.css';
-
+import Image from 'next/image';
 export default function ImagePlaceholder({loading}) {
   return (
     <>
-      {loading ? <div className={styles.placeholderLoading}></div> : <div className={styles.placeholder}></div>}
+
+      {loading ?  <div><Image  id='src_img' src='/loader-dark.gif' width="384" height="256" alt="" quality={95} /> </div> :
+        <Image id='res_img' src='/blank-dark.png' width="384" height="256" alt="" quality={95}/> }
     </>
   );
 }
+
+// {loading ? <div className={styles.placeholderLoading}></div> : <div className={styles.placeholder}></div>}

c26d32cab1d4c7cc9badee49362c5c43789b61f3
Joshua Peddle <joshuapeddle128@gmail.com>
2023-05-28 21:50:33 -0230

    Remove wrap

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,16 @@
 import { Inter } from 'next/font/google';
 import TFView from '@/components/TFView';
 import TopBar from '@/components/TopBar';
+import Footer from '@/components/Footer';
 const inter = Inter({ subsets: [ 'latin' ] });
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen min-w-screen flex-col items-center justify-center flex-wrap  ${inter.className}`}
+      className={`flex min-h-screen min-w-screen flex-col items-center justify-center  ${inter.className}`}
     >
       <TopBar />
       <TFView />
+
     </main>
   );
 }

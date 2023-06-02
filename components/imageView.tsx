96e735933a43cf774dd2c86c1d34d7a58730d968
Joshua Peddle <joshuapeddle128@gmail.com>
2023-06-02 16:49:19 -0230

    Solve issue with moving layout

--- components/imageView.tsx
+++ components/imageView.tsx
@@ -2,14 +2,15 @@ import Image from 'next/image';
 import ImagePlaceholder from './ImagePlaceholder';
 import styles from './imageView.module.css';
 
-export default function ImageView({image, result, loading, size} : {image: string | null, result: string | null, loading: boolean, size: [number, number]}) {
+export default function ImageView({ image, result, loading, size }: { image: string | null, result: string | null, loading: boolean, size: [number, number] }) {
   return (
     <div className={styles.imageContainer}>
       <div className={styles.images}>
-        {image ? <Image  placeholder="blur" blurDataURL={'/blank-dark.png'}  priority={true}  id='src_img' src={image}  width={size[0]} height={size[1]} quality={85} alt="" loader={({ src }) => {
-          return src; 
-        }} unoptimized /> : <ImagePlaceholder loading={true} size={size}/>}
-        {result ? <Image  placeholder="blur" blurDataURL={'/blank-dark.png'} id='res_img' src={result}  width={size[0]} height={size[1]} alt="" quality={85} /> : <ImagePlaceholder loading={loading} size={size}/>}
+        {image ? <Image placeholder="blur" blurDataURL={'/blank-dark.png'} priority={true} id='src_img' src={image} width={size[0]} height={size[1]} quality={85} alt="" loader={({ src }) => {
+          return src;
+        }} unoptimized /> : <Image placeholder="blur" blurDataURL={'/blank-dark.png'} id='res_img' src={'/loader-dark.gif'} width={size[0]} height={size[1]} alt="" quality={85} />}
+        {result ? <Image placeholder="blur" blurDataURL={'/blank-dark.png'} id='res_img' src={result} width={size[0]} height={size[1]} alt="" quality={85} /> :
+          <Image placeholder="blur" blurDataURL={'/blank-dark.png'} id='res_img' src={'/loader-dark.gif'} width={size[0]} height={size[1]} alt="" quality={85} />}
       </div>
     </div>
   );

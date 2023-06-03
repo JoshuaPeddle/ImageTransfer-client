e3406a1da8feb8806481352d329d0609391de211
Joshua Peddle <joshuapeddle128@gmail.com>
2023-06-03 02:19:38 -0230

    Make logo file smaller

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image';
-import Link from 'next/link';
 import styles from './TopBar.module.css';
 export default function TopBar() {
   return (
@@ -7,7 +6,7 @@ export default function TopBar() {
       <div className={styles.topbar}>
         <div className={styles.topbar_inner}>
           <div className={styles.topbar_logo}>
-            <Image src="/logo.png" width="100" height="100" alt="Logo" />
+            <Image src="/logo.png" width={100} height={100} placeholder='empty' quality={10} alt="Logo" />
           </div>
           <div className={styles.topbar_title}>
             styleswap.art

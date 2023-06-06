051739c6e88cc4200eb9df8afa37221dcd855866
Joshua Peddle <joshuapeddle128@gmail.com>
2023-06-06 13:45:24 -0230

    Pass id

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -4,17 +4,20 @@ import GithubProvider from 'next-auth/providers/github';
 import { createUser, getUser } from '@/lib/User';
 export const authOptions = {
   callbacks: {
-    async jwt({ token }:any) {
+    async jwt({ token, account, profile  }:any) {
       // Initial sign in
+      console.log('Token: ', token, 'Account: ', account, 'Profile: ', profile);
       const user = await getUser(token.email);
       if (user) {
         token.num_tokens = user.num_tokens;
+        token.id = user.uuid;
         return token;
       }
       // User doesnt exist, create them
       const new_user = await createUser(token.email, token.name);
       if (new_user) {
         token.num_tokens = new_user.num_tokens;
+        token.id = new_user.uuid;
         return token;
       }
       console.log('Error creating user');
@@ -23,6 +26,7 @@ export const authOptions = {
     async session({ session, token }:any) {
       // Add the user id to the session right after signin
       session.user.num_tokens = token.num_tokens;
+      session.user.uuid = token.id;
       return session;
     }
   },

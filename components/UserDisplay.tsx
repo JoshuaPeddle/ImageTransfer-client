490689372bd03685e6d680cb66a2a00cc2cb8be3
Joshua Peddle <joshuapeddle128@gmail.com>
2023-06-06 15:57:01 -0230

    Styling

--- components/UserDisplay.tsx
+++ components/UserDisplay.tsx
@@ -4,15 +4,15 @@ import { signIn, signOut } from 'next-auth/react';
 export default function UserDisplay({ loggedIn, name, num_tokens }: { loggedIn: Session | null, name: string | null | undefined, num_tokens: number | undefined }) {
   if (loggedIn) {
     return (
-      <div className='flex'>
-        <h1>{num_tokens}</h1>
+      <div className='flex flex-col'>
+        <h1>{num_tokens} Tokens </h1>
         <button  onClick={() => signOut()}>Sign-Out</button>
       </div>
     );
   }
   return (
     <>
-      Not signed in <br />
+      
       <button onClick={() => signIn()}>Sign in</button>
     </>
   );

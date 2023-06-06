c456a5946f3dc605902f862040fcca3fd5ae7a2b
Joshua Peddle <joshuapeddle128@gmail.com>
2023-06-06 16:03:05 -0230

    fix wrap behavior

--- components/UserDisplay.tsx
+++ components/UserDisplay.tsx
@@ -4,16 +4,17 @@ import { signIn, signOut } from 'next-auth/react';
 export default function UserDisplay({ loggedIn, name, num_tokens }: { loggedIn: Session | null, name: string | null | undefined, num_tokens: number | undefined }) {
   if (loggedIn) {
     return (
-      <div className='flex flex-col'>
+      <div className='flex flex-row flex-wrap justify-end pl-3'>
         <h1>{num_tokens} Tokens </h1>
-        <button  onClick={() => signOut()}>Sign-Out</button>
+        
+        <button className='underline pl-3' onClick={() => signOut()}>Log Out</button>
       </div>
     );
   }
   return (
     <>
       
-      <button onClick={() => signIn()}>Sign in</button>
+      <button onClick={() => signIn()}>Log in</button>
     </>
   );
 }

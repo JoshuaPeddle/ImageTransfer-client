bb14d32e103fe28f53ac4ea7f2e1849fff25cedd
Joshua Peddle <joshuapeddle128@gmail.com>
2023-06-06 13:44:17 -0230

    Add a method to charge the user tokens

--- lib/User.ts
+++ lib/User.ts
@@ -4,6 +4,7 @@ export interface User {
     name: string | null;
     email: string | null;
     num_tokens: number;
+    uuid: string;
 }
 
 export async function doesUserExist(email: string): Promise<boolean> {
@@ -26,7 +27,8 @@ export async function createUser(email: string, name: string): Promise<User> {
   const user: User = {
     name: userRec.name,
     email: userRec.email,
-    num_tokens: userRec.num_tokens
+    num_tokens: userRec.num_tokens,
+    uuid: userRec.id
   };
   return user;
 }
@@ -43,7 +45,24 @@ export async function getUser(email: string): Promise<User | null> {
   const user: User = {
     name: userRec.name,
     email: userRec.email,
-    num_tokens: userRec.num_tokens
+    num_tokens: userRec.num_tokens,
+    uuid: userRec.id
+
   };
   return user;
 }
+
+export async function chargeToken(uuid :string): Promise<number | null> {
+  const user = await prisma.user.update({
+    where: {
+      id: uuid
+    },
+    data: {
+      num_tokens: {
+        increment: -1
+      }
+    }
+  });
+  return user.num_tokens;
+}
+

48659348b5fc371a22ca14704be13c95b2aa626e
Joshua Peddle <joshuapeddle128@gmail.com>
2023-06-06 13:33:00 -0230

    add uuid to user interface

--- types/next-auth.ts
+++ types/next-auth.ts
@@ -6,7 +6,8 @@ declare module 'next-auth' {
   interface Session {
     user: {
       /** The user's postal address. */
-      num_tokens: number
+      num_tokens: number,
+      uuid: string,
     } & DefaultSession['user']
   }
 }

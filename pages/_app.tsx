60638faf38038f0508f0a60ec23f73f72ac8e16e
Joshua Peddle <joshuapeddle128@gmail.com>
2023-06-06 13:44:35 -0230

    Pass in appProps type

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
+import { Session } from 'next-auth';
 export default function App({
   Component,
-  pageProps: { session, ...pageProps },
-} : AppProps) {
+  pageProps,
+} : AppProps<{session:Session}>) {
   return (
-    <SessionProvider session={session}>
+    <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
     </SessionProvider>
   );

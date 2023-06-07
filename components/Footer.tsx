a86e1715c1fef6a3753ca4fec351fd9590b6e12c
Joshua Peddle <joshuapeddle128@gmail.com>
2023-06-07 01:49:14 -0230

    Use next Link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,18 +1,17 @@
 import styles from './Footer.module.css';
-
+import Link from 'next/link';
 export default function footer() {
   return (
     <>
       <div className={styles.footer}> 
         <div className={styles.footer_inner}>
           <div className={styles.footer_info}>
-            <p className={styles.footer_title}>styleswap.art</p>
             <p className={styles.footer_quote}>Style Transfer for Everyone</p>
             <p className={styles.footer_year}>2023</p>
           </div>
           <div className={styles.footer_links}>
-            <a href="/privacy">Privacy Policy</a>
-            <a href="/TOS">TOS</a>
+            <Link href="/privacy">Privacy Policy</Link>
+            <Link href="/TOS">TOS</Link>
           </div>
         </div>
       </div>

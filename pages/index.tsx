70f8be3083be8fb6e9d05336eeb2c290f18dcea3
Joshua Peddle <joshuapeddle128@gmail.com>
2023-06-06 22:55:21 -0230

    Add time to refresh request

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,10 @@ export default function Home() {
     const init_num_tokens = () => {
       const last_request = window.localStorage.getItem('last_request');
       const num_tokens = window.localStorage.getItem('num_tokens');
-      if (!last_request) return window.localStorage.setItem('num_tokens', '10');
+      if (!last_request) {
+        window.localStorage.setItem('num_tokens', '10');
+        return window.localStorage.setItem('last_request', new Date().toString());
+      }
       if (last_request && num_tokens) {                 // Refresh num_tokens if last request was more than 24 hours ago
         const now = new Date();
         const last = new Date(last_request);

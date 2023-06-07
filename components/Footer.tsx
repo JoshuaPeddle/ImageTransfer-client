82fe9741b4dee523850fda4dfd92ee5c13ef83e7
Joshua Peddle <joshuapeddle128@gmail.com>
2023-06-07 01:44:27 -0230

    Add links to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,10 +5,14 @@ export default function footer() {
     <>
       <div className={styles.footer}> 
         <div className={styles.footer_inner}>
-          <div className={styles.footer_title}>
-            <h1>styleswap.art</h1>
+          <div className={styles.footer_info}>
+            <p className={styles.footer_title}>styleswap.art</p>
+            <p className={styles.footer_quote}>Style Transfer for Everyone</p>
+            <p className={styles.footer_year}>2023</p>
           </div>
           <div className={styles.footer_links}>
+            <a href="/privacy">Privacy Policy</a>
+            <a href="/TOS">TOS</a>
           </div>
         </div>
       </div>

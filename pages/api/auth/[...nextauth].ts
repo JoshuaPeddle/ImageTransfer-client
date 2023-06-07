dffcea150f6025831b0c71d9797ed030767e24c2
Joshua Peddle <joshuapeddle128@gmail.com>
2023-06-07 00:26:30 -0230

    Add google auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,6 +1,6 @@
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
-
+import GoogleProvider from 'next-auth/providers/google';
 import { createUser, getUser } from '@/lib/User';
 export const authOptions = {
   callbacks: {
@@ -35,6 +35,11 @@ export const authOptions = {
       clientId: process.env.GITHUB_ID || '',
       clientSecret: process.env.GITHUB_SECRET || '',
     }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID || '',
+      clientSecret: process.env.GOOGLE_SECRET || '',
+    }),
+    
     // ...add more providers here
   ],
 };

9f6827636d1d511feb885ccc41327a5fd38d0df3
Joshua Peddle <joshuapeddle128@gmail.com>
2023-06-14 07:29:07 -0230

    Properly center images on small display

--- components/imageView.tsx
+++ components/imageView.tsx
@@ -1,6 +1,7 @@
 import dynamic from 'next/dynamic';
 import styles from './imageView.module.css';
 import { useCallback, useEffect, useRef, useState} from 'react';
+import { Box } from '@mui/material';
 const Image = dynamic(() => import('next/image'));
 export default function ImageView({ image, result, loading, size }: { image: string | null, result: string | null, loading: boolean, size: [number, number] }) {
   const imageStyle = {
@@ -24,18 +25,18 @@ export default function ImageView({ image, result, loading, size }: { image: str
   }, [ calcHeight ]);
 
   return (
-    <div className={styles.imageContainer}>
-      <div className={styles.images}>
+
+    <Box sx={{display:'flex', justifyContent:'center', flexDirection: 'row', flexWrap:'wrap'}}>
  
-        <div ref={src_ref} style={{display:'flex', minWidth:'min(384px,100vw)', maxWidth:'min(384px,100vw)'}}> 
-          <Image onLoadingComplete={calcHeight} style={imageStyle} placeholder="blur"  blurDataURL={'/blank-dark.png'} priority={true} id='src_img' src={image? image :'/loader-dark.gif'}    width={size[0]} height={size[1]}  quality={85} alt="" loader={({ src }) => {
-            return src;
-          }} unoptimized />
-        </div>
-        {<div style={{ display:'flex',  minWidth:'min(384px,100vw)', maxWidth:'min(384px,100vw)', height:height}}>
-          <Image style={imageStyle} placeholder="blur" blurDataURL={'/blank-dark.png'} id='res_img' src={loading? '/loader-dark.gif' : result? result :'/blank-dark.png'} width={size[0]} height={size[1]} alt="" quality={85} />
-        </div> }
-      </div>
-    </div>
+      <Box ref={src_ref} sx={{display:'flex', minWidth:'min(384px,100vw)', maxWidth:'min(384px,100vw)'}}> 
+        <Image onLoadingComplete={calcHeight} style={imageStyle} placeholder="blur"  blurDataURL={'/blank-dark.png'} priority={true} id='src_img' src={image? image :'/loader-dark.gif'}    width={size[0]} height={size[1]}  quality={85} alt="" loader={({ src }) => {
+          return src;
+        }} unoptimized />
+      </Box>
+      <Box style={{ display:'flex',  minWidth:'min(384px,100vw)', maxWidth:'min(384px,100vw)', height:height}}>
+        <Image style={imageStyle} placeholder="blur" blurDataURL={'/blank-dark.png'} id='res_img' src={loading? '/loader-dark.gif' : result? result :'/blank-dark.png'} width={size[0]} height={size[1]} alt="" quality={85} />
+      </Box> 
+    </Box>
+ 
   );
 }  

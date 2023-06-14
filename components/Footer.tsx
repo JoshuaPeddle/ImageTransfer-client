1948096235350c2863ff1306c34fa5a254b2f960
Joshua Peddle <joshuapeddle128@gmail.com>
2023-06-14 11:55:33 -0230

    Adjust shadow

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,16 @@ import { AppBar, Box, Button, Grid } from '@mui/material';
 export default function footer() {
   return (
   
-    <AppBar sx={{  flexDirection:'row', flexGrow:0, justifyContent:'flex-end', alignItems: 'center'}} color='transparent' position='relative' >
+    <AppBar 
+      sx={{  
+        flexDirection:'row', 
+        flexGrow:0,     
+        backgroundColor: 'rgb(255,255,255,0.02)',
+        justifyContent:'flex-end', 
+        alignItems: 'center', 
+        boxShadow: '0px 2px 4px -1px rgba(0,0,0,0.2), 0px 4px 5px 0px rgba(0,0,0,0.14), 0px 1px 3px 3px rgba(0,0,0,0.2)'}} 
+      color='transparent' 
+      position='relative' >
       <Box sx={{p:'2px'}}>
         <Link href="/privacy">
           <Button variant="text" color="primary">

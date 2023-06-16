b3367cf916d91f6ba7039753199aedfd21402638
Joshua Peddle <joshuapeddle128@gmail.com>
2023-06-16 19:48:47 -0230

    Add caching to model route

--- components/Slider.tsx
+++ components/Slider.tsx
@@ -54,6 +54,7 @@ const SimpleSlider: React.FC<Props> = ({ items }) => {
         backgroundColor: 'rgb(255,255,255,0.02)',
         boxShadow: '0px 2px 4px -1px rgba(0,0,0,0.2), 0px 4px 5px 0px rgba(0,0,0,0.14), 0px 1px 10px 0px rgba(0,0,0,0.12)',
         width: '100%',
+        minWidth: '100%',
         pt: '30px',
         pb: '30px',
         px: '30px',

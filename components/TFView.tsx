1b91fda07f47e740f198dfead9a496360fe140b6
Joshua Peddle <joshuapeddle128@gmail.com>
2023-06-19 16:15:49 -0230

    Wait for previous random image before allowing another

--- components/TFView.tsx
+++ components/TFView.tsx
@@ -1,5 +1,5 @@
 import dynamic from 'next/dynamic';
-import { useEffect, useState, useCallback } from 'react';
+import { useEffect, useState, useCallback, useRef } from 'react';
 
 import { nextVariant, Model } from '../lib/models';
 import _predict from '../lib/predict';
@@ -39,11 +39,7 @@ export default function TFView() {
   const [ compressed, setCompressed ] = useState(null as null | Blob);
   const [ exportPopup, setExportPopup ] = useState(false);
   const [ sourceImageSize, setSourceImageSize ] = useState([ 384, 256 ] as [number, number]);
-  useEffect(() => {
-    if (!image) return;
-    generateUUID().then(setUuid);
-    compressImage(image, setCompressed);
-  }, [ image ]);
+  const fetchingRandomImage = useRef(false);
   const predict = async (model: string) => {
     if (!compressed) return;
     if (loading) return;
@@ -63,6 +59,8 @@ export default function TFView() {
     setResult(null);
   };
   const fetchRandomImage = useCallback(async () => {
+    if (fetchingRandomImage.current == true) return;
+    fetchingRandomImage.current = true;
     // This function sends send a GET request to the generator server to get a url for a random image
     const prefetchImage = async (url: string) => {
       const img = new Image();
@@ -75,7 +73,8 @@ export default function TFView() {
     prefetchImage(data.url);
     setSourceImageSize([ 512, 341 ]);
     _setImage(data.url);
-  }, []);
+    fetchingRandomImage.current = false;
+  }, [  ]);
   const _open_export_popup = () => {
     setExportPopup(true);
   };
@@ -90,9 +89,12 @@ export default function TFView() {
     fetchRandomImage();
   }, [ fetchRandomImage ]);
   const _setImage = (img: string | null) => {
-    setImage(img);
     setResult(null);
     setCompressed(null);
+    if (!img) return;
+    setImage(img);
+    generateUUID().then(setUuid);
+    compressImage(img, setCompressed);
   };
   return (
     <>
@@ -103,7 +105,7 @@ export default function TFView() {
           <Instructions/>
         </Box>
         <Box sx={{ display: 'flex', flexGrow: 1, flexDirection: 'column', justifyContent: 'center', alignItems: 'center', maxWidth: 'fit-content', mx: 'auto' }} color='transparent'>
-          <EditorButtons _setImage={_setImage} fetchRandomImage={fetchRandomImage} resultToImage={resultToImage} result={result} loading={loading} _open_export_popup={_open_export_popup} setSourceImageSize={setSourceImageSize} />
+          <EditorButtons _setImage={_setImage} fetchRandomImage={fetchRandomImage} resultToImage={resultToImage} result={result} loading={loading} _open_export_popup={_open_export_popup} setSourceImageSize={setSourceImageSize} fetchingRandomImage={fetchingRandomImage.current} />
           <ImageView image={image} result={result} loading={loading} size={sourceImageSize} />
         </Box >
         <Box sx={{ width: 'min(86vw, 1200px)', maxWidth: '1200px', flexGrow: 1, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>

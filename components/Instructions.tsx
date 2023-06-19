d7501b696b21106f7e408778cec22b398c43b631
Joshua Peddle <joshuapeddle128@gmail.com>
2023-06-19 17:12:18 -0230

    Style change

--- components/Instructions.tsx
+++ components/Instructions.tsx
@@ -9,23 +9,26 @@ import {
 
 export default function Instructions() {
   return (
-    <Accordion>
+    <Accordion sx={{maxWidth:'96vw'}}>
       <AccordionSummary
         expandIcon={<ExpandMoreIcon />}
         aria-controls="panel1a-content"
         id="panel1a-header"
+        sx={{'&	.MuiAccordionSummary-content':{margin:'6px 0'}}}
       >
-        <Typography>Instructions: </Typography>
+        <Typography variant='button'>Instructions </Typography>
       </AccordionSummary>
       <Card variant="outlined">
-        <CardContent>
-          <div >
-            <ol >
-              <li>Upload an image</li>
-              <li>Choose a style from the Style Selector</li>
-              <li>Export as a GIF or MP4</li>
-            </ol>
-          </div>
+        <CardContent sx={{'&:last-child':{paddingBottom:'16px'}}}> 
+          
+          <ol style={{margin:'0px', paddingLeft:'15px'}}>
+            
+            <li><Typography variant='body2'>Upload an image</Typography></li>
+            <li><Typography variant='body2'>Choose a style from the Style Selector below</Typography></li>
+            <li><Typography variant='body2'>Export as an Image, GIF, or MP4</Typography></li>
+
+          </ol>
+      
         </CardContent>
       </Card>
     </Accordion>
